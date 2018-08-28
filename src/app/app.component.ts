9b93917255a76d6975ead19a66aba9fd33638876
Gilberto Stankiewicz <hello@kiewic.com>
2018-08-27 23:45:00 -0700

    Color rows by date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,18 @@ declare module "moment" {
 }
 
 interface HourRow {
+  date: string;
   local: string;
   seattle: string;
   china?: string;
   korea?: string;
   japan?: string;
+  dateClass: number;
+  localClass: number;
+  seattleClass: number;
+  chinaClass: number;
+  koreaClass: number;
+  japanClass: number;
 }
 
 @Component({
@@ -40,28 +47,53 @@ export class AppComponent {
     const startTime = moment.utc("2018-08-31T19:00:00Z")
     const endTime = moment.utc("2018-09-24T18:35:00Z");
 
-    this.createRange(startTime, endTime);
+    const momentRange: Array<moment.Moment> = this.createMomentRange(startTime, endTime);
+    this.createRange(momentRange);
 
     console.log(this.range.length);
   }
 
-  private createRange(startTime: moment.Moment, endTime: moment.Moment) {
-    this.range = [];
+  private createMomentRange(startTime: moment.Moment, endTime: moment.Moment): Array<moment.Moment> {
+    const range: Array<moment.Moment> = [];
     const currentTime = startTime.clone();
 
     // Make sure the last hour is fully covered by the range by adding one hour.
     const finalTime = endTime.clone().add(1, 'h');
 
     while (currentTime < finalTime) {
-      console.log(currentTime, 'vs', finalTime);
+      range.push(currentTime.clone());
+      currentTime.add(1, 'h');
+    }
 
+    return range;
+  }
+
+  private createRange(momentRange: Array<moment.Moment>) {
+    this.range = [];
+
+    for (const currentTime of momentRange) {
       // List of time zones: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+      const local = currentTime.local();
+      const seattle = currentTime.clone().tz("America/Los_Angeles");
+      const china = currentTime.clone().tz("Asia/Shanghai");
+      const korea = currentTime.clone().tz("Asia/Seoul");
+      const japan = currentTime.clone().tz("Asia/Tokyo");
+
+      const min = moment.min(local, seattle, china, korea, japan);
+
       this.range.push({
-        local: currentTime.local().format('llll'),
-        seattle: currentTime.clone().tz("America/Los_Angeles").format('llll'),
-        china: currentTime.clone().tz("Asia/Shanghai").format('llll'),
-        korea: currentTime.clone().tz("Asia/Seoul").format('llll'),
-        japan: currentTime.clone().tz("Asia/Tokyo").format('llll'),
+        date: min.format('ll'),
+        local: local.format('LTS'),
+        seattle: seattle.format('LTS'),
+        china: china.format('LTS'),
+        korea: korea.format('LTS'),
+        japan: japan.format('LTS'),
+        dateClass: min.dayOfYear() % 2,
+        localClass: local.dayOfYear() % 2,
+        seattleClass: seattle.dayOfYear() % 2,
+        chinaClass: china.dayOfYear() % 2,
+        koreaClass: korea.dayOfYear() % 2,
+        japanClass: japan.dayOfYear() % 2,
       });
       currentTime.add(1, 'h');
     }
